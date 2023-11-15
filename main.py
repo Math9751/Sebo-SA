24f07130cd623c1213e948b0eac5c867011017df
Math9751 <matthewbelt.mc@gmail.com>
2023-11-15 20:24:12 -0300

    Sebo SA

---
+++ main.py
@@ -0,0 +1,62 @@
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='H@pp!n3$',
+    database='db_sebo',
+)
+
+cursor = conexao.cursor()
+
+# CREATE itens
+titulo = 'O Corvo'
+autor = 'Edgar Allan Poe'
+preco = 58.90
+descricao = 'Poema'
+status = 1
+periodicidade = 'mensal'
+dataEdicao = 20201214
+area = 4
+vendedor = 2
+categoria = 4
+comando = f'INSERT INTO itens (titulo, autor, preco, descricao, status, periodicidade, dataEdicao, area, userID, categoriaID) VALUES ("{titulo}", "{autor}", {preco}, "{descricao}", {status}, "{periodicidade}", {dataEdicao}, {area}, {vendedor}, {categoria})'
+cursor.execute(comando)
+conexao.commit() #edita o banco de dados
+resultado = cursor.fetchall() #lê o banco de dados
+
+# CREATE users
+# nome = 
+# email = 
+# senha = 
+# tipo = 
+# status =
+# comando = f'INSERT INTO users (nome, email, senha, tipo, tipoID, status) VALUES ("Matheus Silva", "matheus.correia@aluno.ifsp.edu.br", SHA2("63557", 256), "Vendedor", 2, 1)' #cadastro do usuário no banco de dados com senha criptografa com hash de 256 bytes
+# cursor.execute(comando)
+# conexao.commit() #edita o banco de dados
+# resultado = cursor.fetchall() #lê o banco de dados
+
+# READ transações
+# comando = f'SELECT * FROM transacions where userId = 1'
+# cursor.execute(comando)
+# resultado = cursor.fetchall() #lê o banco de dados
+# print(resultado)
+
+
+
+# UPDATE
+# nome_produto = "iphone"
+# valor = 6000
+# comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
+# cursor.execute(comando)
+# conexao.commit()
+
+
+# DELETE
+# nome_produto = "iphone"
+# comando = f'DELETE FROM vendas WHERE nome_produto = "{nome_produto}"'
+# cursor.execute(comando)
+# conexao.commit()
+
+cursor.close()
+conexao.close()
