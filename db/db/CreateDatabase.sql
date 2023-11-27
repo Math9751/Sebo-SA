9f452b7b98c5672bd11d567e0bafd9272a1d85a2
Math9751 <matthewbelt.mc@gmail.com>
2023-11-27 18:56:06 -0300

    adicionados itens e categorias

---
+++ db/db/CreateDatabase.sql
@@ -0,0 +1,9 @@
+USE db_sebo;
+
+INSERT INTO itens (titulo, autor, preco, descricao, periodicidade, dataEdicao, userID, categoriaID)
+VALUES ('Crime e Castigo', 'Fiódor Dostoiévski', 49.90, 'Um clássico do autor, escrito durante seu exílio na Sibéria', 'bimestral', 2022-11-29, 3, 6);
+INSERT INTO itens (titulo, autor, preco, descricao, periodicidade, dataEdicao, userID, categoriaID)
+VALUES ('Vidas Secas', 'Graciliano Ramos', 44.47, 'Um retrato da dura vida dos retirantes nordestinos', 'trimestral', 2019-2-4, 1, 6);
+INSERT INTO itens (titulo, autor, preco, descricao, periodicidade, dataEdicao, userID, categoriaID)
+VALUES ('As Aventuras de Tom Sawyer', 'Mark Twain', 19.90, '
+Tom Sawyer mora com a tia e o irmão em um vilarejo do sul dos Estados Unidos. Com o amigo Huckleberry Finn, o garoto diverte-se bastante e vive grandes aventuras.', 'semestral', 2019-9-24, 3, 3);
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import Flask, make_response, jsonify, request
 import mysql.connector
 
 conexao = mysql.connector.connect(
@@ -7,56 +8,195 @@ conexao = mysql.connector.connect(
     database='db_sebo',
 )
 
-cursor = conexao.cursor()
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False 
 
-# # CREATE itens
-# titulo = 'O Corvo'
-# autor = 'Edgar Allan Poe'
-# preco = 58.90
-# descricao = 'Poema'
-# status = 1
-# periodicidade = 'mensal'
-# dataEdicao = 20201214
-# area = 4
-# vendedor = 2
-# categoria = 4
-# comando = f'INSERT INTO itens (titulo, autor, preco, descricao, status, periodicidade, dataEdicao, area, userID, categoriaID) VALUES ("{titulo}", "{autor}", {preco}, "{descricao}", {status}, "{periodicidade}", {dataEdicao}, {area}, {vendedor}, {categoria})'
-# cursor.execute(comando)
-# conexao.commit() #edita o banco de dados
-# resultado = cursor.fetchall() #lê o banco de dados
+@app.route('/itens', methods=['GET'])
+def get_itens():
 
-# CREATE users
-# nome = 
-# email = 
-# senha = 
-# tipo = 
-# status =
-# comando = f'INSERT INTO users (nome, email, senha, tipo, tipoID, status) VALUES ("Matheus Silva", "matheus.correia@aluno.ifsp.edu.br", SHA2("63557", 256), "Vendedor", 2, 1)' #cadastro do usuário no banco de dados com senha criptografa com hash de 256 bytes
-# cursor.execute(comando)
-# conexao.commit() #edita o banco de dados
-# resultado = cursor.fetchall() #lê o banco de dados
+    cursor = conexao.cursor()
+    read = f"SELECT * FROM itens WHERE status = 0"
+    cursor.execute(read)
+    meus_itens = cursor.fetchall()
 
-# READ transações
-# comando = f'SELECT * FROM transacions where userId = 1'
-# cursor.execute(comando)
-# resultado = cursor.fetchall() #lê o banco de dados
-# print(resultado)
+    itens = list()
+    for item in meus_itens:
+        itens.append(
+            {
+                'itemID': item[0],
+                'titulo': item[1],
+                'autor': item[2],
+                'preco': item[3],
+                'descricao': item[4],
+                'periodicidade': item[5],
+                'dataEdicao': item[6],
+                'userID': item[7],
+                'categoriaID': item[8],
+                'status': item[9]
+            }
+        )
 
-
-
-# UPDATE
-titulo = "O Corvo"
-preco = 54.90
-comando = f'UPDATE itens SET preco = {preco} WHERE titulo = "{titulo}"'
-cursor.execute(comando)
-conexao.commit()
+    return make_response(
+        jsonify (
+            mensagem='Lista de itens.',
+            dados=itens
+            )
+        )
 
 
-# DELETE
-# titulo = "O Corvo"
-# comando = f'DELETE FROM itens WHERE titulo = "{titulo}"'
-# cursor.execute(comando)
-# conexao.commit()
+@app.route('/itens', methods=['POST'])
+def create_item():
+    try:
+        # Obtenha os dados do corpo da solicitação como JSON
+        item = request.get_json()
 
-cursor.close()
-conexao.close()
+        # Adicione o novo item à lista
+        cursor = conexao.cursor()
+        create = f"INSERT INTO itens (titulo, autor, preco, descricao, periodicidade, dataEdicao, userID, categoriaID) VALUES ('{item['titulo']}', '{item['autor']}', '{item['preco']}', '{item['descricao']}', '{item['periodicidade']}', '{item['dataEdicao']}', '{item['userID']}', '{item['categoriaID']}')"
+        cursor.execute(create)
+        conexao.commit()
+
+        # Retorne o item criado
+        return jsonify(
+            mensagem='Livro adicionado com sucesso.',
+            álbum=item
+            ), 201  # 201 Created
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400  # 400 Bad Request
+    
+@app.route('/itens', methods=['PUT'])
+def update_item():
+    try:
+        # Obtenha os dados do corpo da solicitação como JSON
+        item = request.get_json()
+
+        # Atualiza o item 
+        cursor = conexao.cursor()
+
+        update = f"UPDATE itens SET titulo = '{item['titulo']}', autor = '{item['autor']}', preco = '{item['preco']}', descricao = '{item['descricao']}', periodicidade = '{item['periodicidade']}', dataEdicao = '{item['dataEdicao']}', userID = '{item['userID']}', categoriaID = '{item['categoriaID']}', WHERE id = '{item['itemID']}'"
+        cursor.execute(update)
+        conexao.commit()
+
+        # Retorne o item criado
+        return jsonify(
+            mensagem='Livro atualizado com sucesso.',
+            álbum=item
+            ), 201  # 201 Created
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400  # 400 Bad Request
+
+@app.route('/itens', methods=['DELETE'])
+def delete_item():
+    try:
+        # Obtenha os dados do corpo da solicitação como JSON
+        item = request.get_json()
+
+        # Deleta o registro do item mas mantem os dados no banco de dados
+        cursor = conexao.cursor()
+
+        delete = f"UPDATE itens SET status = 1 WHERE titulo = '{item['titulo']}'"
+        cursor.execute(delete)
+        conexao.commit()
+
+        # Confirma a exclusão da categoria
+        return jsonify(
+            mensagem='Livro excluído com sucesso.'
+            ), 201  # 201 Created
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400  # 400 Bad Request
+
+@app.route('/categorias', methods=['GET'])
+def get_categorias():
+
+    cursor = conexao.cursor()
+    read = f"SELECT * FROM categorias WHERE status = 0"
+    cursor.execute(read)
+    meus_categorias = cursor.fetchall()
+
+    categorias = list()
+    for categoria in meus_categorias:
+        categorias.append(
+            {
+                'categoriaID': categoria[0],
+                'nomeCategoria': categoria[1],
+                'status': categoria[2]
+            }
+        )
+
+    return make_response(
+        jsonify (
+            mensagem='Lista de categorias.',
+            dados=categorias
+            )
+        )
+
+
+@app.route('/categorias', methods=['POST'])
+def create_categoria():
+    try:
+        # Obtenha os dados do corpo da solicitação como JSON
+        categoria = request.get_json()
+
+        # Adicione a nova categoria à lista
+        cursor = conexao.cursor()
+        create = f"INSERT INTO categorias (nomecategoria) VALUES ('{categoria['nomeCategoria']}')"
+        cursor.execute(create)
+        conexao.commit()
+
+        # Retorna a categoria criada
+        return jsonify(
+            mensagem='Categoria adicionada com sucesso.',
+            álbum=categoria
+            ), 201  # 201 Created
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400  # 400 Bad Request
+    
+@app.route('/categorias', methods=['PUT'])
+def update_categoria():
+    try:
+        # Obtenha os dados do corpo da solicitação como JSON
+        categoria = request.get_json()
+
+        # Atualiza a categoria
+        cursor = conexao.cursor()
+
+        update = f"UPDATE categorias SET nomeCategoria = '{categoria['nomeCategoria']}' WHERE id = '{categoria['categoriaID']}'"
+        cursor.execute(update)
+        conexao.commit()
+
+        # Retorne a categoria criada
+        return jsonify(
+            mensagem='Categoria atualizada com sucesso.',
+            álbum=categoria
+            ), 201  # 201 Created
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400  # 400 Bad Request
+
+@app.route('/categorias', methods=['DELETE'])
+def delete_categoria():
+    try:
+        # Obtenha os dados do corpo da solicitação como JSON
+        categoria = request.get_json()
+
+         # Deleta o registro da categoria mas mantem os dados no banco de dados
+        cursor = conexao.cursor()
+
+        delete = f"UPDATE categorias SET status = 1 WHERE nomeCategoria = '{categoria['nomeCategoria']}'"
+        cursor.execute(delete)
+        conexao.commit()
+
+        # Confirma a exclusão da categoria
+        return jsonify(
+            mensagem='Categoria excluída com sucesso.'
+            ), 201  # 201 Created
+
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400  # 400 Bad Request
+
+if __name__ == '__main__':
+    app.run(debug=True)
